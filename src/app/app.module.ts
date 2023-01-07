33b92b6469234712c6701352f518c5f78e765cd4
Elybourland <JEBourland@yahoo.com>
2023-01-07 15:59:59 -0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './components/alerts/success-alert/success-alert.component';
+import { WarningAlertComponent } from './components/alerts/warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
